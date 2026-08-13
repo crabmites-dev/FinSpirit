12d2cce37fc1418ba57744fbd1d482ed55556576
Mmor <mmbow6355@gmail.com>
2026-08-13 15:32:59 +0000

    Ajout scripts commit Git sans attribution Cursor

---
+++ scripts/git-commit-sans-cursor.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal EnableDelayedExpansion
+cd /d "%~dp0.."
+
+set GIT_AUTHOR_NAME=Mmor
+set GIT_AUTHOR_EMAIL=mmbow6355@gmail.com
+set GIT_COMMITTER_NAME=Mmor
+set GIT_COMMITTER_EMAIL=mmbow6355@gmail.com
+
+set "MSG=%*"
+if "!MSG!"=="" set "MSG=Mise a jour"
+
+git add -A
+for /f %%i in ('git rev-parse HEAD') do set PARENT=%%i
+for /f %%i in ('git write-tree') do set TREE=%%i
+(echo !MSG!)> "%TEMP%\git-msg.txt"
+for /f %%i in ('git commit-tree !TREE! -p !PARENT! -F "%TEMP%\git-msg.txt"') do set COMMIT=%%i
+del "%TEMP%\git-msg.txt"
+
+if "!COMMIT!"=="" exit /b 1
+
+git update-ref refs/heads/main !COMMIT!
+git reset --hard main
+git log -1 --format=full
